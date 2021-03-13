eb0444d1c044510bc3416f6262228d60125885a4
hokorobi <hokorobi.hokorobi@gmail.com>
2021-03-13 20:00:23 +0900

    output

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
@@ -13,6 +17,18 @@ var (
 	rowStartTask    = 2
 	colStartTargets = 20
 	colCyclic       = 3
+	colPeriod       = 4
+	colTitle        = 5
+	colDetail       = 6
+
+	iCyclic = 0
+	iPeriod = 1
+	iTitle  = 2
+	iDetail = 3
+	weekday = map[string]int{"日": 0, "月": 1, "火": 2, "水": 3, "木": 4, "金": 6, "土": 6}
+
+	start = time.Date(2021, 3, 13, 0, 0, 0, 0, time.Local)
+	end   = time.Date(2022, 3, 13, 0, 0, 0, 0, time.Local)
 )
 
 func main() {
@@ -40,31 +56,123 @@ func _main() error {
 	targets := getTargets(sheet)
 	log.Println(targets)
 
-	fmt.Println(getTargetSchedule(sheet, 1))
+	fmt.Println(getPlainTasks(sheet, 2))
+	writeCsv(convertTask(getPlainTasks(sheet, 2)))
 	return nil
 }
 
-func getTargetSchedule(s [][]string, indexTarget int) [][]string {
-	var schedules [][]string
-	var schedule []string
+func getPicks(task []string) ([]time.Time, string) {
+	var picks []time.Time
+	var cycle string
+	if task[iCyclic] == "年" {
+		r := regexp.MustCompile(`\d+`)
+		result := r.FindAllStringSubmatch(task[iPeriod], -1)
+		for _, period := range result {
+			month, err := strconv.Atoi(period[0])
+			if err != nil {
+				log.Println(err)
+			}
+			picks = append(picks, time.Date(start.Year(), time.Month(month), 1, 0, 0, 0, 0, time.Local))
+		}
+		cycle = "Y"
+	} else if task[iCyclic] == "月" {
+		r := regexp.MustCompile(`\d+`)
+		result := r.FindAllStringSubmatch(task[iPeriod], -1)
+		for _, period := range result {
+			day, err := strconv.Atoi(period[0])
+			if err != nil {
+				log.Println(err)
+			}
+			picks = append(picks, time.Date(start.Year(), start.Month(), day, 0, 0, 0, 0, time.Local))
+		}
+		cycle = "M"
+	} else if task[iCyclic] == "週" {
+		var diff int
+		r := regexp.MustCompile(`[月火水木金土日]`)
+		result := r.FindAllStringSubmatch(task[iPeriod], -1)
+		for _, period := range result {
+			current_weekday := weekday[period[0]]
+			if int(start.Weekday()) <= current_weekday {
+				diff = current_weekday - int(start.Weekday())
+			} else {
+				diff = current_weekday - int(start.Weekday()) + 7
+			}
+			picks = append(picks, start.AddDate(0, 0, diff))
+		}
+		cycle = "W"
+	} else {
+		return nil, ""
+	}
+	return picks, cycle
+}
+
+func writeCsv(records [][]string) {
+	f, err := os.Create("import.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := csv.NewWriter(f)
+	w.WriteAll(records)
+	w.Flush()
+
+	if err := w.Error(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func convertTask(ts [][]string) [][]string {
+	var tasks [][]string
+	var header = []string{"ユーザー/グループシステムID", "氏名/グループ名", "ＩＤ（システムＩＤ：自動発番）", "開始日", "開始時刻", "終了日", "終了時刻", "予定", "件名", "場所", "内容", "プライベート", "外出区分", "重要度", "予約種別", "帯状", "フラグ", "アイコン番号", "承認依頼", "確認通知メール"}
+	tasks = append(tasks, header)
+
+	for _, t := range ts {
+		picks, cycle := getPicks(t)
+		if cycle == "" {
+			continue
+		}
+		for _, pick := range picks {
+			for pick.Unix() <= end.Unix() {
+				task := make([]string, 20, 20)
+				task[3] = pick.Format("2006-01-02")
+				task[4] = "8:30"
+				task[5] = pick.Format("2006-01-02")
+				task[6] = "8:30"
+				task[8] = t[iTitle]
+				task[10] = t[iDetail]
+				task[19] = ""
+				tasks = append(tasks, task)
+				if cycle == "Y" {
+					pick = pick.AddDate(1, 0, 0)
+				} else if cycle == "M" {
+					pick = pick.AddDate(0, 1, 0)
+				} else {
+					// cycle == "W"
+					pick = pick.AddDate(0, 0, 7)
+				}
+			}
+		}
+
+	}
+	return tasks
+}
+
+func getPlainTasks(s [][]string, indexTarget int) [][]string {
+	var tasks [][]string
+	var task []string
 	colTarget := colStartTargets + indexTarget
 
-	for i, cols := range s {
+	for i, cells := range s {
 		if i < rowStartTask {
 			continue
 		}
 		// fmt.Println(cols[colTarget])
-		if cols[colTarget] != "〇" {
+		if cells[colTarget] != "〇" {
 			continue
 		}
-		for j, cell := range cols {
-			if j == colCyclic {
-				schedule = []string{cell}
-			}
-		}
-		schedules = append(schedules, schedule)
+		task = []string{cells[colCyclic], cells[colPeriod], cells[colTitle], cells[colDetail]}
+		tasks = append(tasks, task)
 	}
-	return schedules
+	return tasks
 }
 
 // 担当者の配列を取得
