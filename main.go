81179c965d1a2c7c2dc59f08c907d3f65aba7b52
hokorobi <hokorobi.hokorobi@gmail.com>
2021-03-13 17:20:14 +0900

    get col

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
+var (
+	rowTargets      = 1
+	rowStartTask    = 2
+	colStartTargets = 20
+	colCyclic       = 3
+)
+
 func main() {
 	if err := _main(); err != nil {
 		fmt.Println(err)
@@ -27,17 +34,41 @@ func _main() error {
 	}
 	// sheets := getSheets(f)
 	sheet := getSheet(f, "Sheet1")
-	printCols2(sheet)
+	// printCols2(sheet)
 	// fmt.Println(getStartColumn())
 
 	targets := getTargets(sheet)
 	log.Println(targets)
+
+	fmt.Println(getTargetSchedule(sheet, 1))
 	return nil
 }
 
+func getTargetSchedule(s [][]string, indexTarget int) [][]string {
+	var schedules [][]string
+	var schedule []string
+	colTarget := colStartTargets + indexTarget
+
+	for i, cols := range s {
+		if i < rowStartTask {
+			continue
+		}
+		// fmt.Println(cols[colTarget])
+		if cols[colTarget] != "〇" {
+			continue
+		}
+		for j, cell := range cols {
+			if j == colCyclic {
+				schedule = []string{cell}
+			}
+		}
+		schedules = append(schedules, schedule)
+	}
+	return schedules
+}
+
+// 担当者の配列を取得
 func getTargets(s [][]string) []string {
-	rowTargets := 1
-	colStartTargets := 20
 	var targets []string
 	for i, cols := range s {
 		if i != rowTargets {
